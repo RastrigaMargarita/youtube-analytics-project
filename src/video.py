7530ee89a8512721a9ec0893568609a9fec6c700
Margarita Rastriga <rasm@1Ci.com>
2023-06-16 00:34:00 +0300

    Playlist class developed

--- src/video.py
+++ src/video.py
@@ -1,4 +1,4 @@
-from src.utils import Utils
+from src.youtube_service import YouTube_Service
 
 
 class Video:
@@ -8,12 +8,13 @@ class Video:
         """Экземпляр инициализируется id видео.
         Дальше все данные будут подтягиваться по API."""
         self.__video_id = video_id
-        youtube_service = Utils.get_service()
+        youtube_service = YouTube_Service.get_service()
         video_response = youtube_service.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                        id=self.__video_id
                                                        ).execute()["items"][0]
 
         self.video_title: str = video_response['snippet']['title']
+        self.url: str = "https://youtu.be/" + video_id
         self.view_count: int = video_response['statistics']['viewCount']
         self.like_count: int = video_response['statistics']['likeCount']
         self.comment_count: int = video_response['statistics']['commentCount']
@@ -27,9 +28,9 @@ class PLVideo(Video):
     """Класс для плейлиста"""
 
     def __init__(self, video_id: str, play_list_id: str) -> None:
-        """Экземпляр инициализируется id канала. """
+        """Экземпляр инициализируется id прейлиста и id видео. """
         super().__init__(video_id)
-        youtube_service = Utils.get_service()
+        youtube_service = YouTube_Service.get_service()
         self.play_list = youtube_service.playlistItems().list(playlistId=play_list_id,
                                                               part='contentDetails',
                                                               maxResults=50,
--- src/youtube_service.py
+++ src/youtube_service.py
@@ -4,8 +4,8 @@ from googleapiclient.discovery import build
 IP_KEY_NAME = 'YT_API_KEY'
 
 
-class Utils:
-    """Класс для общих методов"""
+class YouTube_Service:
+    """Класс для сервиса методов"""
 
     @classmethod
     def get_service(cls):

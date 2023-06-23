9d3a53165605cac1c73dec4fcdcc9c2b844d43ce
Margarita Rastriga <rasm@1Ci.com>
2023-06-24 01:36:27 +0300

    Обработка неверного айди

--- src/video.py
+++ src/video.py
@@ -9,19 +9,27 @@ class Video:
         Дальше все данные будут подтягиваться по API."""
         self.__video_id = video_id
         youtube_service = YouTube_Service.get_service()
-        video_response = youtube_service.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+        try:
+            video_response = youtube_service.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                        id=self.__video_id
                                                        ).execute()["items"][0]
 
-        self.video_title: str = video_response['snippet']['title']
-        self.url: str = "https://youtu.be/" + video_id
-        self.view_count: int = video_response['statistics']['viewCount']
-        self.like_count: int = video_response['statistics']['likeCount']
-        self.comment_count: int = video_response['statistics']['commentCount']
+            self.title: str = video_response['snippet']['title']
+            self.url: str = "https://youtu.be/" + video_id
+            self.view_count: int = video_response['statistics']['viewCount']
+            self.like_count: int = video_response['statistics']['likeCount']
+            self.comment_count: int = video_response['statistics']['commentCount']
+        except Exception:
+            self.title: str = None
+            self.url: str = None
+            self.view_count: int = None
+            self.like_count: int = None
+            self.comment_count: int = None
+
 
     def __str__(self):
         """Возвращает представление видео для пользователя"""
-        return f"{self.video_title}"
+        return f"{self.title}"
 
 
 class PLVideo(Video):
